5ed4469b21c20f16ec7fb38cedb9a0b4cc37aa1f
Zahlul <zahlul.2001@gmail.com>
2024-12-05 22:27:30 +0700

    user management ui done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,29 @@ class HomeController extends Controller
     {
         return view('dashboard');
     }
+
+    public function users()
+    {
+        return view('user-management.users');
+    }
+
+    public function users_view()
+    {
+        return view('user-management.users_view');
+    }
+
+    public function roles()
+    {
+        return view('user-management.roles');
+    }
+
+    public function roles_view()
+    {
+        return view('user-management.roles_view');
+    }
+
+    public function permissions()
+    {
+        return view('user-management.permissions');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,14 @@ Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('dashboard');
+    Route::name('user-management.')->group(function () {
+        Route::get('/user-management/users', [HomeController::class, 'users'])->name('users');
+        Route::get('/user-management/users/view', [HomeController::class, 'users_view'])->name('users_view');
+        Route::get('/user-management/roles', [HomeController::class, 'roles'])->name('roles');
+        Route::get('/user-management/roles/view', [HomeController::class, 'roles_view'])->name('roles_view');
+        Route::get('/user-management/permissions', [HomeController::class, 'permissions'])->name('permissions');
+        // Route::resource('/user-management/users', HomeController::class);
+        // Route::resource('/user-management/roles', HomeController::class);
+        // Route::resource('/user-management/permissions', HomeController::class);
+    });
 });
