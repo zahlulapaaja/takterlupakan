03463f871e5ab68ca2311babdd84997b6cbd145d
Zahlul <zahlul.2001@gmail.com>
2024-12-07 12:09:12 +0700

    edit action button

--- resources/views/user-management/users.blade.php
+++ resources/views/user-management/users.blade.php
@@ -229,28 +229,13 @@
                         </td>
                         <!-- <td></td> -->
                         <td>{{ $user->email_verified_at }}</td>
-                        <td class="text-end">
-                            <a href="#" class="btn btn-light btn-active-light-primary btn-flex btn-center btn-sm" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
-                                <i class="ki-duotone ki-down fs-5 ms-1"></i></a>
-                            <!--begin::Menu-->
-                            <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4" data-kt-menu="true">
-                                <!--begin::Menu item-->
-                                <div class="menu-item px-3">
-                                    <a href="{{ route('user-management.users.show', ['user' => $user->id]) }}" class="menu-link px-3">Edit</a>
-                                </div>
-                                <!--end::Menu item-->
-                                <!--begin::Menu item-->
-                                <div class="menu-item px-3">
-                                    <form class="menu-link px-3" method="POST" action="{{ route('user-management.users.destroy', ['user' => $user->id]) }}" data-kt-users-table-filter="delete_row">
-                                        @method('DELETE')
-                                        @csrf
-                                        <input style="all: unset" type="submit" value="Delete" />
-                                    </form>
-                                    <!-- <a href="{{ route('user-management.users.destroy', ['user' => $user->id]) }}" class="menu-link px-3" data-kt-users-table-filter="delete_row">Delete</a> -->
-                                </div>
-                                <!--end::Menu item-->
-                            </div>
-                            <!--end::Menu-->
+                        <td class="d-flex align-items-center">
+                            <a href="{{ route('user-management.users.show', ['user' => $user->id]) }}" class="btn btn-primary btn-active-primary btn-flex btn-center btn-sm mx-1 my-2">Edit</a>
+                            <form class="btn btn-danger btn-active-danger-primary btn-flex btn-center btn-sm mx-1 my-2" method="POST" action="{{ route('user-management.users.destroy', ['user' => $user->id]) }}">
+                                @method('DELETE')
+                                @csrf
+                                <input style="all: unset" type="submit" value="Delete" />
+                            </form>
                         </td>
                     </tr>
                     @empty
