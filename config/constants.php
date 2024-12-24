8dce3de3bcacd6ce51ff465fb99fbb0ae3548441
Zahlul <zahlul.2001@gmail.com>
2024-12-24 09:35:07 +0700

    update constants dan menu sidebar

--- config/constants.php
+++ config/constants.php
@@ -2,9 +2,11 @@
 
 return [
 
-    'INSTANSI' => 'Badan Pusat Statistik',
-    'SATKER' => 'BPS Kabupaten Aceh Barat',
-    'MEULABOH' => 'Meulaboh',
+    // nama satuan kerja
+    'INSTANSI'   => 'Badan Pusat Statistik',
+    'SATKER'     => 'BPS Kabupaten Aceh Barat',
+    'MEULABOH'   => 'Meulaboh',
+    'PENGADILAN' => 'Pengadilan Negeri Meulaboh',
 
 
     // alamat
@@ -13,6 +15,15 @@ return [
     'KODEPOS'   => '23617',
     'TELP'      => '(62-655) 7553330',
     'EMAIL'     => 'bps1107@bps.go.id',
-    'WEBSITE'   => 'https://acehbaratkab.bps.go.id/'
+    'WEBSITE'   => 'https://acehbaratkab.bps.go.id/',
+
+    // limit honor
+    'LIMIT_HONOR_PENDATAAN_SURVEI' => 3439000,
+    'LIMIT_HONOR_PEMERIKSAAN_SURVEI' => 3439000,
+    'LIMIT_HONOR_PENGOLAHAN_SURVEI' => 3414000,
+    'LIMIT_HONOR_PENDATAAN_SENSUS' => 4815000,
+    'LIMIT_HONOR_PEMERIKSAAN_SENSUS' => 5330000,
+    'LIMIT_HONOR_PENGOLAHAN_SENSUS' => 3525000,
+    'LIMIT_HONOR_PENGAWASAN_OLAH_SENSUS' => 3525000,
 
 ];
--- resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
+++ resources/views/layout/partials/sidebar-layout/sidebar/_menu.blade.php
@@ -72,40 +72,6 @@
 							<span class="path4"></span>
 						</i>
 					</span>
-					<span class="menu-title">Sementara</span>
-					<span class="menu-arrow"></span>
-				</span>
-				<!--end:Menu link-->
-				<!--begin:Menu sub-->
-				<div class="menu-sub menu-sub-accordion">
-					<!--begin:Menu item-->
-					<div class="menu-item">
-						<!--begin:Menu link-->
-						<a class="menu-link" href="{{ route('master.mitra.impor') }}">
-							<span class="menu-bullet">
-								<span class="bullet bullet-dot"></span>
-							</span>
-							<span class="menu-title">Impor Mitra</span>
-						</a>
-						<!--end:Menu link-->
-					</div>
-					<!--end:Menu item-->
-				</div>
-				<!--end:Menu sub-->
-			</div>
-			<!--end:Menu item-->
-			<!--begin:Menu item-->
-			<div data-kt-menu-trigger="click" class="menu-item menu-accordion">
-				<!--begin:Menu link-->
-				<span class="menu-link">
-					<span class="menu-icon">
-						<i class="ki-duotone ki-dollar fs-2">
-							<span class="path1"></span>
-							<span class="path2"></span>
-							<span class="path3"></span>
-							<span class="path4"></span>
-						</i>
-					</span>
 					<span class="menu-title">Anggaran</span>
 					<span class="menu-arrow"></span>
 				</span>
@@ -204,6 +170,8 @@
 						<i class="ki-duotone ki-menu fs-2">
 							<span class="path1"></span>
 							<span class="path2"></span>
+							<span class="path3"></span>
+							<span class="path4"></span>
 						</i>
 					</span>
 					<span class="menu-title">Matriks</span>
