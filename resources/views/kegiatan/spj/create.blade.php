51ba504ed496a299e1fe33d0f97ef6f2de6341a2
Zahlul <zahlul.2001@gmail.com>
2025-01-15 23:53:07 +0700

    perbaikan tampilan card di create edit spj

--- resources/views/kegiatan/spj/create.blade.php
+++ resources/views/kegiatan/spj/create.blade.php
@@ -14,117 +14,121 @@
         @method('POST')
         <!--begin::Aside column-->
         <div class="d-flex flex-column flex-lg-row-fluid gap-7 gap-lg-10">
-            <!--begin::Order details-->
-            <div class="card card-flush py-4">
-                <!--begin::Card header-->
-                <div class="card-header">
-                    <div class="card-title">
-                        <h2>Detail Kegiatan</h2>
+
+            <div class="d-flex flex-column flex-lg-row gap-7 gap-lg-10">
+                <!--begin::MAK-->
+                <div class="card card-flush py-4 w-full">
+                    <!--begin::Card header-->
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h2>Detail Kegiatan</h2>
+                        </div>
                     </div>
-                </div>
-                <!--end::Card header-->
-                <!--begin::Card body-->
-                <div class="card-body pt-0">
-                    <div class="d-flex flex-column gap-10">
-                        <!--begin::Input group-->
-                        <div class="fv-row">
-                            <input type="hidden" name="kegiatans_id" value="{{$keg->id}}" />
-                            <input type="hidden" name="akun" value="{{$keg->pok->kode_akun}}" />
-                            <input type="hidden" name="tahun" value="{{$keg->pok->tahun}}" />
-                            <!--begin::MAK-->
-                            <div class="d-flex flex-column flex-lg-row py-auto gap-5">
-                                <label class="w-32 font-semibold fs-6">MAK</label>
-                                <div class="fw-semibold fs-6 bg-cyan-300">{{ $keg->mak }}</div>
-                            </div>
-                            <!--end::MAK-->
-                            <!--begin::Nama Kegiatan-->
-                            <div class="d-flex flex-column flex-lg-row py-auto gap-5">
-                                <label class="w-32 font-semibold fs-6">Nama Kegiatan</label>
-                                <div class="fw-semibold fs-6">{{ $keg->nama }}</div>
-                            </div>
-                            <!--end::Nama Kegiatan-->
-                            <!--begin::Jadwal Kegiatan-->
-                            <div class="d-flex flex-column flex-lg-row py-auto gap-5">
-                                <label class="text-nowrap w-32 font-semibold fs-6">Jadwal Kegiatan</label>
-                                <div class="fw-semibold fs-6">{{$keg->tgl_mulai}} s.d. {{$keg->tgl_akhir}}</div>
-                            </div>
-                            <!--end::Jadwal Kegiatan-->
-                            <!--begin::PJK-->
-                            <div class="d-flex flex-column flex-lg-row py-auto gap-5">
-                                <label class="w-32 font-semibold fs-6">PJK</label>
-                                <div class="fw-semibold fs-6">{{$keg->pjk->nama}}</div>
-                            </div>
-                            <!--end::PJK-->
-                            <!--begin::Detil Kegiatan-->
-                            <div class="d-flex flex-column flex-lg-row py-auto gap-5">
-                                <label class="w-32 font-semibold fs-6">Detil Kegiatan</label>
-                                <div class="fw-semibold fs-6">{{ $keg->pok->item_kegiatan }}</div>
-                            </div>
-                            <!--end::Detil Kegiatan-->
-                            <!--begin::Anggaran-->
-                            <div class="d-flex flex-column flex-lg-row py-auto gap-5">
-                                <label class="w-32 font-semibold fs-6">Anggaran</label>
-                                <div class="fw-semibold fs-6 d-flex flex-column flex-lg-row">
-                                    <span class="mr-8">[Volume : {{ $keg->pok->volume }}]</span>
-                                    <span class="mr-8">[Satuan : {{ $keg->pok->satuan }}]</span>
-                                    <span>[Jumlah : {{ $keg->pok->jumlah }}]</span>
+                    <!--end::Card header-->
+                    <!--begin::Card body-->
+                    <div class="card-body pt-0">
+                        <div class="d-flex flex-column gap-10">
+                            <!--begin::Input group-->
+                            <div class="fv-row">
+                                <input type="hidden" name="kegiatans_id" value="{{$keg->id}}" />
+                                <input type="hidden" name="akun" value="{{$keg->pok->kode_akun}}" />
+                                <input type="hidden" name="tahun" value="{{$keg->pok->tahun}}" />
+                                <!--begin::MAK-->
+                                <div class="d-flex flex-column flex-lg-row py-auto gap-x-5">
+                                    <label class="w-32 font-semibold fs-6 text-nowrap">MAK</label>
+                                    <div class="fw-semibold fs-6 ms-4 me-auto bg-cyan-300">{{ $keg->mak }}</div>
                                 </div>
-                            </div>
-                            <!--end::Anggaran-->
-                        </div>
-                        <!--end::Input group-->
-                    </div>
-                </div>
-                <!--end::Card header-->
-            </div>
-            <!--end::Order details-->
-
-            <div class="card card-flush py-4">
-                <!--begin::Card header-->
-                <div class="card-header">
-                    <div class="card-title">
-                        <h2>Tanggal SPJ</h2>
-                    </div>
-                </div>
-                <!--end::Card header-->
-                <!--begin::Card body-->
-                <div class="card-body pt-0">
-                    <div class="d-flex flex-column gap-5">
-                        <!--begin::Input group-->
-                        <div class="fv-row">
-                            <label class="required form-label">Input SPJ</label>
-                            <input type="date" class="form-control" name="tgl" required />
-                        </div>
-                        <!--end::Input group-->
-
-                        @if($keg->pok->kode_akun == config('constants.AKUN_TRANSLOK'))
-                        <div class="d-flex flex-column flex-md-row gap-5">
-                            <!--begin::Input group-->
-                            <div class="w-full w-lg-1/2 fv-row flex-row-fluid">
-                                <label class="required form-label">Nomor Surat Tugas</label>
-                                <input name="no_st" type="text" list="nomot_st" class="form-control mb-2" placeholder="Masukkan Nomor Surat..." required />
-                                <div class="text-muted fs-7">Buat surat tugas di menu Nomor Surat - Tim Kerja</div>
-                                <datalist id="nomot_st">
-                                    @foreach($nomor_st as $n)
-                                    <option value="{{$n->no_surat}}">{{$n->no_surat}}</option>
-                                    @endforeach
-                                </datalist>
-                            </div>
-                            <!--end::Input group-->
-                            <!--begin::Input group-->
-                            <div class="w-full w-lg-1/2 fv-row flex-row-fluid">
-                                <label class="required form-label">Tanggal Surat Tugas</label>
-                                <input type="date" name="tgl_st" class="form-control mb-2" required />
+                                <!--end::MAK-->
+                                <!--begin::Nama Kegiatan-->
+                                <div class="d-flex flex-column flex-lg-row py-auto gap-x-5">
+                                    <label class="w-32 font-semibold fs-6 text-nowrap">Nama Kegiatan</label>
+                                    <div class="fw-semibold fs-6 pl-4">{{ $keg->nama }}</div>
+                                </div>
+                                <!--end::Nama Kegiatan-->
+                                <!--begin::Jadwal Kegiatan-->
+                                <div class="d-flex flex-column flex-lg-row py-auto gap-x-5">
+                                    <label class="text-nowrap w-32 font-semibold fs-6 text-nowrap">Jadwal Kegiatan</label>
+                                    <div class="fw-semibold fs-6 pl-4">{{$keg->tgl_mulai}} s.d. {{$keg->tgl_akhir}}</div>
+                                </div>
+                                <!--end::Jadwal Kegiatan-->
+                                <!--begin::PJK-->
+                                <div class="d-flex flex-column flex-lg-row py-auto gap-x-5">
+                                    <label class="w-32 font-semibold fs-6 text-nowrap">PJK</label>
+                                    <div class="fw-semibold fs-6 pl-4">{{$keg->pjk->nama}}</div>
+                                </div>
+                                <!--end::PJK-->
+                                <!--begin::Detil Kegiatan-->
+                                <div class="d-flex flex-column flex-lg-row py-auto gap-x-5">
+                                    <label class="w-32 font-semibold fs-6 text-nowrap">Detil Kegiatan</label>
+                                    <div class="fw-semibold fs-6 pl-4">{{ $keg->pok->item_kegiatan }}</div>
+                                </div>
+                                <!--end::Detil Kegiatan-->
+                                <!--begin::Anggaran-->
+                                <div class="d-flex flex-column flex-lg-row py-auto gap-x-5">
+                                    <label class="w-32 font-semibold fs-6 text-nowrap">Anggaran</label>
+                                    <div class="fw-semibold fs-6 pl-4 d-flex flex-column flex-lg-row">
+                                        <span class="mr-8">[Volume : {{ $keg->pok->volume }}]</span>
+                                        <span class="mr-8">[Satuan : {{ $keg->pok->satuan }}]</span>
+                                        <span>[Jumlah : {{ $keg->pok->jumlah }}]</span>
+                                    </div>
+                                </div>
+                                <!--end::Anggaran-->
                             </div>
                             <!--end::Input group-->
                         </div>
-                        @endif
-
                     </div>
+                    <!--end::Card header-->
                 </div>
-                <!--end::Card header-->
+                <!--end::MAK-->
+
+                <!--begin::No SPJ details-->
+                <div class="card card-flush py-4 w-full">
+                    <!--begin::Card header-->
+                    <div class="card-header">
+                        <div class="card-title">
+                            <h2>Tanggal SPJ</h2>
+                        </div>
+                    </div>
+                    <!--end::Card header-->
+                    <!--begin::Card body-->
+                    <div class="card-body pt-0">
+                        <div class="d-flex flex-column gap-5">
+                            <!--begin::Input group-->
+                            <div class="fv-row">
+                                <label class="required form-label">Tanggal Input SPJ</label>
+                                <input type="date" class="form-control" name="tgl" required />
+                            </div>
+                            <!--end::Input group-->
+
+                            @if($keg->pok->kode_akun == config('constants.AKUN_TRANSLOK'))
+                            <div class="d-flex flex-column flex-md-row gap-5">
+                                <!--begin::Input group-->
+                                <div class="w-full w-lg-1/2 fv-row flex-row-fluid">
+                                    <label class="required form-label">Nomor Surat Tugas</label>
+                                    <input name="no_st" type="text" list="nomot_st" class="form-control mb-2" placeholder="Masukkan Nomor Surat..." required />
+                                    <!-- <div class="text-muted fs-7">Buat surat tugas di menu Nomor Surat - Tim Kerja</div> -->
+                                    <datalist id="nomot_st">
+                                        @foreach($nomor_st as $n)
+                                        <option value="{{$n->no_surat}}">{{$n->no_surat}}</option>
+                                        @endforeach
+                                    </datalist>
+                                </div>
+                                <!--end::Input group-->
+                                <!--begin::Input group-->
+                                <div class="w-full w-lg-1/2 fv-row flex-row-fluid">
+                                    <label class="required form-label">Tanggal Surat Tugas</label>
+                                    <input type="date" name="tgl_st" class="form-control mb-2" required />
+                                </div>
+                                <!--end::Input group-->
+                            </div>
+                            @endif
+
+                        </div>
+                    </div>
+                    <!--end::Card header-->
+                </div>
+                <!--end::No SPJ details-->
             </div>
-            <!--end::No SPJ details-->
 
             <!--begin::Petugas-->
             <div class="card card-flush py-4">
@@ -217,7 +221,7 @@
             <!--end::Petugas-->
             <div class="d-flex justify-content-end">
                 <!--begin::Button-->
-                <a href="{{ route('pok.index') }}" id="form_create_spj_cancel" class="btn btn-light me-5">Kembali</a>
+                <a href="{{ route('kegiatan.index') }}" id="form_create_spj_cancel" class="btn btn-light me-5">Kembali</a>
                 <!--end::Button-->
                 <!--begin::Button-->
                 <button type="submit" id="form_create_spj_submit" class="btn btn-primary">
